e6a76825cd66d798b9103d7113d871f45c5c4dc9
Jadhav-Krishna <krishnajadhav85017@gmail.com>
2023-08-29 01:43:13 +0530

    link css

---
+++ FirstProgram.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int t2,n;
+	    scanf("%d",&t2);
+	    scanf("%d",&n);
+	    while(t2--){
+	        int x;
+	        scanf("%d",&x);
+	        if(x>=n){
+                int c=0;
+	            while(x1>0){ 
+                    
+                    x1=x/10;
+                    c++;
+
+                }
+                    printf("%d\n",c);
+	            
+	        }
+	    }
+	}
+	return 0;
+}
